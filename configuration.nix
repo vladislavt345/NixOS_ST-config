838ceb147b1421ebfab977292e07a340ae8c96b3
vladislavt345 <vladislavt30515@gmail.com>
2026-03-11 23:07:08 +0300

    hypr-start

--- configuration.nix
+++ configuration.nix
@@ -44,6 +44,16 @@
   };
 
   programs.hyprland.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
   programs.ssh = {
     startAgent = true;
     extraConfig = ''
